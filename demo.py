d634e675e95eda5309bc092059448aa10ce9395f
Kirill Voronin <yoursdearboy@gmail.com>
2022-12-08 13:04:44 +0000

    Reimplemented using htmx

--- demo.py
+++ demo.py
@@ -1,71 +1,48 @@
-from uuid import uuid4
-from blinker import Namespace
 import jinja2
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
 # from flask_wtf import FlaskForm
-from werkzeug.datastructures import MultiDict
 from wtforms import Form, StringField
 from wtforms.validators import DataRequired
 
 
 app = Flask(__name__, static_folder='', template_folder='')
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
-forms = dict()
-signals = Namespace()
 
 
-class OnlineForm:
-    def __init__(self, *args, oid=None, template=None, **kwargs):
-        super(OnlineForm, self).__init__(*args, **kwargs)
-
-        oid = str(uuid4()) if oid is None else oid
-        self.oid = oid
-        forms[oid] = self
-
-        self.template = template
+class HTMXForm:
+    def __init__(self, *args, **kwargs):
+        super(HTMXForm, self).__init__(*args, **kwargs)
 
     def render(self):
         return jinja2.utils.markupsafe.Markup(render_template(self.template, form=self))
 
 
-class MyForm(OnlineForm, Form):
-    name = StringField('name', validators=[DataRequired()])
+class MyForm(HTMXForm, Form):
+    id = 'my_form'
+    template="my_form.html"
+    render_kw = {
+        "hx-trigger": "keyup changed delay:1000ms",
+        "hx-post": "/wtforms_htmx",
+        "hx-swap": "outerHTML",
+        "hx-target": f"#{id}"
+    }
+
+    name = StringField('name', validators=[DataRequired()], render_kw=render_kw)
+    prefix = StringField('prefix', render_kw=render_kw)
 
     @property
     def hello(self):
-        return f"Hello {self.name.data}"
+        return f"Hello {self.prefix.data or ''} {self.name.data or 'stranger'}"
+
+
+@app.route("/wtforms_htmx", methods=['POST'])
+def wtforms_htmx():
+    form = MyForm(formdata=request.form)
+    return form.render()
 
 
 @app.route('/my_form', methods=['GET', 'POST'])
 def my_form():
-    form = MyForm(name="default", template="my_form.html")
+    form = MyForm(name="default")
     # if form.validate_on_submit():
     #     return redirect('/my_form')
     return render_template('demo.html', form=form)
-
-
-@socketio.on('change')
-def handle_socket_change(event):
-    oid = event['oid']
-    form = forms[oid]
-
-    name = event['name']
-    value = event['value']
-    data = MultiDict([(name, value)])
-    form.process(data)
-
-    signals.signal('change').send(form)
-
-
-# FIXME: fix event loop
-@signals.signal('change').connect
-def handle_signal_change(form):
-    oid = form.oid
-    html = form.render()
-    emit('update', dict(oid=oid, html=html))
-
-
-if __name__ == '__main__':
-    socketio.run(app)
